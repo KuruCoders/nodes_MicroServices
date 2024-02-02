01bee1cbcb98ea8fe90617f8b6a89751b777d07e
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-02-02 07:28:22 +0530

    gateWayStatus

--- gateway/index.js
+++ gateway/index.js
@@ -9,8 +9,10 @@ app.use(express.json())
 // redirecting to the specific services via proxy
 app.use('/customer', proxy('https://micro-customer.onrender.com'))
 app.use('/shopping', proxy('https://micro-shopping.onrender.com'))
-app.use('/',proxy('https://micro-product.onrender.com'))
-
+app.use('/product',proxy('https://micro-product.onrender.com'))
+app.use('/', (req, res, next) => {
+    return res.status(200).json({'msg':'GateWay Online'})
+})
 app.listen(8000, () => {
     console.log('gateway on port 8000')
 })
