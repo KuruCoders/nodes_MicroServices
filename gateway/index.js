5a1c8cad214c5ee183b735c5e4b0d18800bfb14a
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-02-02 07:17:40 +0530

    gateWayUpdated

--- gateway/index.js
+++ gateway/index.js
@@ -7,9 +7,9 @@ app.use(cors())
 app.use(express.json())
 
 // redirecting to the specific services via proxy
-app.use('/customer', proxy('http://localhost:8001'))
-app.use('/shopping', proxy('http://localhost:8003'))
-app.use('/',proxy('http://localhost:8002'))
+app.use('/customer', proxy('https://micro-customer.onrender.com'))
+app.use('/shopping', proxy('https://micro-shopping.onrender.com'))
+app.use('/',proxy('https://micro-product.onrender.com'))
 
 app.listen(8000, () => {
     console.log('gateway on port 8000')
